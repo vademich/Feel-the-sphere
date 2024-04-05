5d6ebb1ef84292b9356962802c34145405f144b8
vademich <goncharov1999vadim@gmail.com>
2024-04-05 17:19:02 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Step 1: run `npm install express --save`
+Step 2: run `node app.js`
+Step 3: open `index.html` in your browser
---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    next();
+});
+
+app.get('/texture', function (req, res) {
+    res.sendFile(__dirname + '/static/textures/NormalMap.png');
+});
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
